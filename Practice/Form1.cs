78a1445fca1e1ba8a435cbe2bd59fb6e754e44e6
Sabina <sabina.orunbaeva@gmail.com>
2018-03-16 22:04:02 +0600

    Add files via upload

---
+++ Practice/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Form1 : Form
+    {
+        private int Port;
+        private Socket srvSock;
+        private Thread srvThread;
+        public Form1()
+        {
+            InitializeComponent();
+            button.Tag = false;
+            button.Text = "Start";
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if((bool)button.Tag == true)
+            {
+                // Отключение сервера
+                if (StopServer()) button.Text = "Start";
+            }
+            else
+            {
+                // Необходимо запустить сервер
+                if (StartServer()) button.Text = "Stop"; 
+            }
+            button.Tag = !(bool)button.Tag;
+        }
+
+        
+
+        private bool StartServer()
+        {
+            Port = 1234;
+            if (srvSock != null) return false;
+
+            srvSock = new Socket(AddressFamily.InterNetwork, 
+                                 SocketType.Stream, 
+                                 ProtocolType.Tcp);
+            IPEndPoint srvEndPoint = new IPEndPoint(0,Port);
+            srvSock.Bind(srvEndPoint);
+            srvSock.Listen(100);
+            ThreadPool.SetMaxThreads(16,32);
+            srvThread = new Thread(SrvRoutine);
+            srvThread.Start();
+            return true;
+        }
+
+        private void SrvRoutine()
+        {
+            Socket clientSocket;
+            while (true)
+            {
+                clientSocket=srvSock.Accept();
+                if (clientSocket == null) { break; }
+                ThreadPool.QueueUserWorkItem(ClientThread, clientSocket);
+            }
+        }
+        private void ClientThread(object state)
+        {
+            Socket clientSocket = (Socket)state;
+            textBox1.Text += "Client connected\n";
+            textBox1.Text += clientSocket.RemoteEndPoint.ToString()+"\n";
+            string str = "Hello!";
+            byte[] bstr = Encoding.ASCII.GetBytes(str);
+            clientSocket.Send(bstr);
+            bstr = new byte[1024];
+            int size=clientSocket.Receive(bstr);
+            str=Encoding.ASCII.GetString(bstr,0,size);
+            textBox1.Text += str;
+            // clientSocket.Shutdown(SocketShutdown.Both);
+        }
+        private bool StopServer()
+        {
+            if (srvSock != null) return false;
+            srvSock.Shutdown(SocketShutdown.Both);
+            return true;
+        }
+    }
+}
---
+++ SP_Lesson3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_Lesson3
+{
+    public partial class Form1 : Form
+    {
+        Mutex mutex;
+        public Form1()
+        {
+            bool IsCreate = true;
+            mutex = new Mutex(false, "MyMutex", out IsCreate);
+            if(!IsCreate)
+                mutex.WaitOne();
+
+            InitializeComponent();
+
+            //if (!IsCreate)
+            //{
+            //    MessageBox.Show("Программа уже запущена");
+            //    Close();
+            //}
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            mutex.ReleaseMutex();
+        }
+    }
+}
